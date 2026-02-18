321dcc425b5e0dbad57bd4ab86db0f5e7b862e07
Jeevika Singathia <singathiajeevika@gmail.com>
2026-02-18 13:00:29 +0530

    Input program which displays 'X' using for loop.

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Hello{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        for(int i = 0;i<a;i++){
+          System.out.println("X");
+        }
+}
+}
